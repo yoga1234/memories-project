693cd075caf2894d0e77035e817fe7b9ce916fa8
Septiana Yoga <septianayoga30@gmail.com>
2021-08-29 19:50:18 +0700

    adding initial route for createPost

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,3 +1,9 @@
+import PostMessage from "../models/postMessage.js";
+
 export const getPosts = (req, res) => {
   res.send("THIS WORKDS");
+};
+
+export const createPost = (req, res) => {
+  res.send("Post Creation");
 };
