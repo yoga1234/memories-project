9bfa07a12c0719240faa4f3d4a839f8ecc3380aa
Septiana Yoga <septianayoga30@gmail.com>
2021-08-30 21:13:38 +0700

    finishing website

--- server/index.js
+++ server/index.js
@@ -2,23 +2,24 @@ import express from "express";
 // import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
+import dotenv from "dotenv";
 
 import postRoutes from "./routes/posts.js";
 
 const app = express();
-
+dotenv.config();
 
 app.use(express.urlencoded({ limit: "30mb", extended: true }));
 app.use(express.json({ limit: "30mb", extended: true }));
 app.use(cors());
 
 app.use("/posts", postRoutes);
-const CONNECTION_URL =
-  "mongodb+srv://septianayoyo:teknikal123@cluster0.vcszv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+// const CONNECTION_URL =
+//   "mongodb+srv://septianayoyo:teknikal123@cluster0.vcszv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 const PORT = process.env.PORT || 5000;
 
 mongoose
-  .connect(CONNECTION_URL, {
+  .connect(process.env.CONNECTION_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
